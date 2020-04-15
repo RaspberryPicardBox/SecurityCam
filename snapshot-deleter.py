1af85e964bbe4935b001f0c1e046f9007345caae
RaspberyPicardBox <raspberrypicardbox@gmail.com>
2020-04-15 17:21:04 +0100

    Update snapshot-deleter.py
    Removed print statements to reduce load on RAM.

--- snapshot-deleter.py
+++ snapshot-deleter.py
@@ -11,8 +11,8 @@ print("Load complete!")
 file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 print("Starting process...")
 for file1 in file_list:
-  print('title: %s, id: %s' % (file1['title'], file1['id']))
+  #print('title: %s, id: %s' % (file1['title'], file1['id']))
   if "Snapshot" in file1['title']:
-        print("Found a Snapshot File. Deleting...")
+        #print("Found a Snapshot File. Deleting...")
         file1.Delete()
 print("Done!")
